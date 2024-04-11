f977011a51ba7c378e723751cfcc67568f41bc74
mhlee <ifthenelse@meta-ent.com>
2024-04-11 12:27:09 +0900

    익명함수 간단표현식으로 개선

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -12,10 +12,10 @@ defmodule Servy.BearController do
     items =
       Wildthings.list_bears()
       ## filter only Grizzly bears
-      |> Enum.filter(fn(b) -> Bear.is_grizzly(b) end)
+      |> Enum.filter(&Bear.is_grizzly(&1))
       ## sort ascending
-      |> Enum.sort(fn(b1, b2) -> Bear.order_asc_by_name(b1, b2) end)
-      |> Enum.map(fn(b) -> bear_item(b) end)
+      |> Enum.sort(&Bear.order_asc_by_name(&1, &2))
+      |> Enum.map(&bear_item(&1))
       |> Enum.join
 
     %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
