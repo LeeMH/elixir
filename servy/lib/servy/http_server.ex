7240a09c241ac108c7c7a406fe29db9211c49c97
mhlee <ifthenelse@meta-ent.com>
2024-04-17 18:03:08 +0900

    erlang 코드 -> elixir 코드로 변환

--- servy/lib/servy/http_server.ex
+++ servy/lib/servy/http_server.ex
@@ -1,18 +1,20 @@
 # code from https://www.erlang.org/doc/man/gen_tcp
 
-server() ->
-  {ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0},
-                                      {active, false}]),
-  {ok, Sock} = gen_tcp:accept(LSock),
-  {ok, Bin} = do_recv(Sock, []),
-  ok = gen_tcp:close(Sock),
-  ok = gen_tcp:close(LSock),
-  Bin.
+# server() ->
+#   {ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0},
+#                                       {active, false}]),
+#   {ok, Sock} = gen_tcp:accept(LSock),
+#   {ok, Bin} = do_recv(Sock, []),
+#   ok = gen_tcp:close(Sock),
+#   ok = gen_tcp:close(LSock),
+#   Bin.
 
-do_recv(Sock, Bs) ->
-  case gen_tcp:recv(Sock, 0) of
-      {ok, B} ->
-          do_recv(Sock, [Bs, B]);
-      {error, closed} ->
-          {ok, list_to_binary(Bs)}
-  end.
+defmodule Servy.HttpServer do
+  def server do
+    {:ok, lsock} = :gen_tcp.listen(5678, [:binary, packet: 0, active: false])
+    {:ok, sock} = :gen_tcp.accept(lsock)
+    {:ok, bin} = :gen_tcp.recv(sock, 0)
+    :ok = :gen_tcp.close(sock)
+    bin
+  end
+end
