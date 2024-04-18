21f9c09f5131e16ce7b9c425cba844f9148bc97f
mhlee <ifthenelse@meta-ent.com>
2024-04-18 19:25:09 +0900

    메세지 박스 full 방지를 위한 디폴트 절 추가

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -1,6 +1,7 @@
 defmodule Servy.PledgeServer do
   @name :pledge_server
 
+  ## Server Side run
   def start do
     IO.puts "Starting the pledge server..."
     pid = spawn(__MODULE__, :listen_loop, [[]])
@@ -24,13 +25,28 @@ defmodule Servy.PledgeServer do
 
         ## 해당 상태를 유지하기 위해, 다시 listen_loop에 해당값을 전달한다.
         listen_loop(new_state)
+
       {sender, :recent_pledges} ->
         send sender, {:response, state}
+        listen_loop(state)
+
+        {sender, :total_pledged} ->
+          total =
+            ## elem은 튜플의 n번째 요소를 가져오는 함수이다. 여기서는 amount를 가져온다.
+            Enum.map(state, &elem(&1, 1))
+            |> Enum.sum
+          send sender, {:response, total}
+          listen_loop(state)
+
+      ## 메세지 박스에 매칭되지 않는 메세지가 계속 쌓이는것을 방지하기 위해 default 절을 추가한다
+      unexpected ->
+        IO.puts "Unexpected messaged: #{inspect unexpected}"
         listen_loop(state)
     end
 
   end
 
+  ## Client Side run
   def create_pledge(name, amount) do
     send @name, {self(), :create_pledge, name, amount}
 
@@ -43,6 +59,12 @@ defmodule Servy.PledgeServer do
     receive do {:response, pledges} -> pledges end
   end
 
+  def total_pledges() do
+    send @name, {self(), :total_pledged}
+
+    receive do {:response, total} -> total end
+  end
+
   defp send_pledge_to_servcie(_name, _amount) do
     # code goes here to send pledge to external service
     {:ok, "pledge-#{:rand.uniform(1000)}"}
@@ -50,14 +72,16 @@ defmodule Servy.PledgeServer do
 end
 
 
-# alias Servy.PledgeServer
+alias Servy.PledgeServer
 
-# PledgeServer.start()
+PledgeServer.start()
 
-# IO.inspect PledgeServer.create_pledge("larry", 10)
-# IO.inspect PledgeServer.create_pledge("moe", 20)
-# IO.inspect PledgeServer.create_pledge("curly", 30)
-# IO.inspect PledgeServer.create_pledge("daisy", 40)
-# IO.inspect PledgeServer.create_pledge("grace", 50)
+IO.inspect PledgeServer.create_pledge("larry", 10)
+IO.inspect PledgeServer.create_pledge("moe", 20)
+IO.inspect PledgeServer.create_pledge("curly", 30)
+IO.inspect PledgeServer.create_pledge("daisy", 40)
+IO.inspect PledgeServer.create_pledge("grace", 50)
 
-# IO.inspect PledgeServer.recent_pledges()
+IO.inspect PledgeServer.recent_pledges()
+
+IO.inspect PledgeServer.total_pledges()
