e2e95098b465dfff2d000af3ddd175f9b0400027
mhlee <ifthenelse@meta-ent.com>
2024-04-18 18:02:58 +0900

    cache 최근 3개만 유지하도록 로직 추가

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -7,14 +7,12 @@ defmodule Servy.PledgeServer do
       {:create_pledge, name, amount} ->
         {:ok, id} = send_pledge_to_servcie(name, amount)
         ## 기존 state 앞에 새로 생성된 pledge를 추가한다.
-        new_state = [ {name, amount} | state ]
-        IO.puts "#{name} pledged #{amount}!"
-        IO.puts "New state is #{inspect new_state}"
+        most_recent_pledges = Enum.take(state, 2)
+        new_state = [ {name, amount} | most_recent_pledges ]
         ## 해당 상태를 유지하기 위해, 다시 listen_loop에 해당값을 전달한다.
         listen_loop(new_state)
       {sender, :recent_pledges} ->
         send sender, {:response, state}
-        IO.puts "Sent pledges to #{inspect sender}"
         listen_loop(state)
     end
 
