f43ce870e4785f76e36c68fb90fc058a691cc1a6
mhlee <ifthenelse@meta-ent.com>
2024-04-19 17:18:34 +0900

    GenServer 전환 및 cache_size 조정 함수 추가

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -4,33 +4,44 @@ defmodule Servy.PledgeServer do
 
   use GenServer
 
+  defmodule State do
+    defstruct cache_size: 3, pledges: []
+  end
+
   def start() do
     IO.puts "Starting the pledge server..."
-    GenServer.start(__MODULE__, [], name: @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
   # server callbacks
-  def handle_cast(:clear, _state) do
-    {:noreply, []}
+  def handle_cast(:clear, state) do
+    {:noreply, %{state | pledges: []}}
   end
+
+  def handle_cast({:set_cache_size, size}, state) do
+    new_state = %{ state | cache_size: size}
+    {:noreply, new_state}
+  end
+
 
   def handle_call(:total_pledged, _from, state) do
     total =
       ## elem은 튜플의 n번째 요소를 가져오는 함수이다. 여기서는 amount를 가져온다.
-      Enum.map(state, &elem(&1, 1))
+      Enum.map(state.pledges, &elem(&1, 1))
       |> Enum.sum
     {:reply, total, state}
   end
 
   def handle_call(:recent_pledges, _from, state) do
-    {:reply, state, state}
+    {:reply, state.pledges, state}
   end
 
   def handle_call({:create_pledge, name, amount}, _from, state) do
     {:ok, id} = send_pledge_to_servcie(name, amount)
     ## 기존 state 앞에 새로 생성된 pledge를 추가한다.
-    most_recent_pledges = Enum.take(state, 2)
-    new_state = [ {name, amount} | most_recent_pledges ]
+    most_recent_pledges = Enum.take(state.pledges, state.cache_size - 1)
+    cache_pledges = [ {name, amount} | most_recent_pledges ]
+    new_state = %{ state | pledges: cache_pledges }
     {:reply, id, new_state}
   end
 
@@ -56,6 +67,10 @@ defmodule Servy.PledgeServer do
     GenServer.cast @name, :clear
   end
 
+  def set_cache_size(size) do
+    GenServer.cast @name, {:set_cache_size, size}
+  end
+
 end
 
 
@@ -63,14 +78,17 @@ alias Servy.PledgeServer
 
 {:ok, pid} = PledgeServer.start()
 
-send pid, {:stop, "hammertime"}
+#send pid, {:stop, "hammertime"}
+
+PledgeServer.set_cache_size(4)
 
 IO.inspect PledgeServer.create_pledge("larry", 10)
+PledgeServer.clear()
 IO.inspect PledgeServer.create_pledge("moe", 20)
 IO.inspect PledgeServer.create_pledge("curly", 30)
 IO.inspect PledgeServer.create_pledge("daisy", 40)
 
-#PledgeServer.clear()
+
 
 IO.inspect PledgeServer.create_pledge("grace", 50)
 
