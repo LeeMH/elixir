4ba38e32c791f5d97e49faa7f159530533ca2ef3
mhlee <ifthenelse@meta-ent.com>
2024-04-22 12:19:12 +0900

    KickStarter 서버 완료

--- servy/lib/servy/kick_starter.ex
+++ servy/lib/servy/kick_starter.ex
@@ -8,10 +8,20 @@ defmodule Servy.KickStarter do
 
   def init(:ok) do
     Process.flag(:trap_exit, true)
-    IO.puts "Starting the HTTP server..."
-    server_pid = spawn(Servy.HttpServer, :start, [4000])
-    Process.link(server_pid)
-    Process.register(server_pid, :http_server)
+    server_pid = start_server()
     {:ok, server_pid}
+  end
+
+  def handle_info({:EXIT, _pid, reason}, _state) do
+    IO.puts "HTTPServer exited (#{inspect reason})"
+    server_pid = start_server()
+    {:noreply, server_pid}
+  end
+
+  defp start_server() do
+    IO.puts "Starting the HTTP server..."
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    Process.register(server_pid, :http_server)
+    server_pid
   end
 end
