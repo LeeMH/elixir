f2b25091c6618b2ddcae5c20d7bab190cb64b45d
mhlee <ifthenelse@meta-ent.com>
2024-04-23 15:03:48 +0900

    dashborad 예제 처리

--- live_view_studio/lib/live_view_studio_web/live/sales_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/sales_live.ex
@@ -4,14 +4,13 @@ defmodule LiveViewStudioWeb.SalesLive do
   alias LiveViewStudio.Sales
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        new_orders: Sales.new_orders(),
-        sales_amount: Sales.sales_amount(),
-        satisfaction: Sales.satisfaction()
-      )
+    ## mount 이벤트가 2번 호출된다. HTML 요청을 받았을때와 socket이 연결되었을때....
+    ## 명시적으로 한번만 처리하기 위해, HTML을 보내고, websocket이 연결되었을때 처리한다.
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :tick)
+    end
 
-    {:ok, socket}
+    {:ok, assign_state(socket)}
   end
 
   def render(assigns) do
@@ -45,10 +44,26 @@ defmodule LiveViewStudioWeb.SalesLive do
         </div>
       </div>
 
-      <button>
+      <button phx-click="refresh">
         <img src="/images/refresh.svg" /> Refresh
       </button>
     </div>
     """
+  end
+
+  def handle_event("refresh", _params, socket) do
+    {:noreply, assign_state(socket)}
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, assign_state(socket)}
+  end
+
+  defp assign_state(socket) do
+    assign(socket,
+      new_orders: Sales.new_orders(),
+      sales_amount: Sales.sales_amount(),
+      satisfaction: Sales.satisfaction()
+    )
   end
 end
