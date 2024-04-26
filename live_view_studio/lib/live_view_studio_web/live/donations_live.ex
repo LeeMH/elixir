6d03fe639d28e55235b085b6d923e83a82a9d503
mhlee <ifthenelse@meta-ent.com>
2024-04-26 10:20:51 +0900

    item sort 기능 추가

--- live_view_studio/lib/live_view_studio_web/live/donations_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/donations_live.ex
@@ -13,4 +13,22 @@ defmodule LiveViewStudioWeb.DonationsLive do
 
     {:ok, socket}
   end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    donations = Donations.list_donations(options)
+    socket =
+      assign(socket,
+        donations: donations
+      )
+
+    {:noreply, socket}
+  end
 end
