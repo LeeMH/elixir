cac899ecc3fd79795c3f4746100d6e461c4563e0
mhlee <ifthenelse@meta-ent.com>
2024-04-29 12:19:01 +0900

    form 입력 처리 추가

--- live_view_studio/lib/live_view_studio_web/live/volunteers_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/volunteers_live.ex
@@ -22,10 +22,10 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
-      <.form for={@form}>
+      <.form for={@form} phx-submit="save">
         <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
         <.input field={@form[:phone]} type="tel" placeholder="Phone" autocomplete="off" />
-        <.button>
+        <.button phx-disable-with="Saving...">
           Check In
         </.button>
       </.form>
@@ -50,5 +50,20 @@ defmodule LiveViewStudioWeb.VolunteersLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
+    case Volunteers.create_volunteer(volunteer_params) do
+      {:ok, volunteer} ->
+        socket =
+          update(socket,
+            :volunteers,
+            &([volunteer | &1])
+          )
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+        {:noreply, assign(socket, :form, to_form(changeset))}
+      {:error, changeset} ->
+        {:noreply, assign(socket, :form, to_form(changeset))}
+    end
   end
 end
